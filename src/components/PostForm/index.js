f2f989bf16b871d22dafcd81e1568a93bac94e1b
Hay Mar Kyaw <mandalaytvsindiamotorcycles@gmail.com>
2024-10-24 14:59:26 +0700

    two way data binding

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -6,6 +6,10 @@ export default function Index() {
 
     let [title,setTitle] = useState("")
 
+    let resetInput = () => {
+        setTitle('');
+    }
+
     // let changeHandler = (e) => {
     //     setTitle(e.target.value)
         
@@ -14,9 +18,10 @@ export default function Index() {
     <form className="post-form">
         <div className="form-control">
             <label>Title</label>
-            <input type='text' className="input-form" onChange={(e)=>setTitle(e.target.value)} />
+            <input type='text' className="input-form" onChange={(e)=>setTitle(e.target.value)} value={title}/>
         </div>
         <p>{title}</p>
+        <button type="submit" onClick={resetInput}>Reset</button>
         <div className="form-control">
             <button>Post Now</button>
         </div>
