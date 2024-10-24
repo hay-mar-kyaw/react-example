8c485c155112fd7f30764bc1d0d1c09e233c1455
Hay Mar Kyaw <mandalaytvsindiamotorcycles@gmail.com>
2024-10-24 11:08:45 +0700

    portal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import './index.css'
+import ReactDOM from 'react-dom';
 
 export default function Modal({children}) {
   return (
-    <div className="modal-backdrop">
+
+    ReactDOM.createPortal(
+        <div className="modal-backdrop">
         <div className="modal">
             {/* modal content  */}
             {children}
         </div>
-    </div>
+    </div>, document.getElementById('modal') )
+    
+    
   )
 }
